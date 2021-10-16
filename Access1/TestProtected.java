ca7a6f55fdd83bd79669dcaacda294abf10dd928
laxmi605 <laxmikonduru@gmail.com>
2021-10-16 19:51:45 +0100

    access

---
+++ Access1/TestProtected.java
@@ -0,0 +1,12 @@
+package Access1;
+
+import Accessmodifier.testc;
+
+ class TestProtected extends testc
+{
+    public static void main(String[] args)
+    {
+        TestProtected t = new TestProtected();
+        t.methodC();
+    }
+}
---
+++ Access1/Testpublic.java
@@ -0,0 +1,11 @@
+package Access1;
+
+import Accessmodifier.testA;
+
+public class Testpublic
+{
+    public static void main(String[] args) {
+        testA A = new testA();
+        A.methodA();
+    }
+}
