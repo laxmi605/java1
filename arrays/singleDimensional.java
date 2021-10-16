32c79b7e20bf9ead884194454f3b9db2802bf7ee
laxmi605 <laxmikonduru@gmail.com>
2021-10-16 19:45:56 +0100

    array

---
+++ arrays/singleDimensional.java
@@ -0,0 +1,28 @@
+package arrays;
+
+import basics.Forloop;
+
+/*public class singleDimensional {
+    public static void main(String[] args)
+    {
+        int [] a = new int[3];
+        a[0] = 124;
+        a[1] = 125;
+        a[2] = 167;
+        System.out.println("length of the array is " + a.length);
+
+        for ( int i = 0; i < a.length ; i++)
+        {
+            System.out.println(a[i]);
+        }
+
+     int [] b = {1,2,3,4,5}; //0-4
+        System.out.println("length of the array is :" +b.length);
+        for (int i=0;i< b.length;i++)
+        {
+            System.out.println(b[i]);
+        }
+
+    }
+
+ */
---
+++ arrays/twodimentional.java
@@ -0,0 +1,17 @@
+/* package arrays;
+
+public class twodimentional
+{
+    public static void main(String[] args)
+    {
+       int [][]a = new int[3][4]//3 rows 4 columns*/
+       // int [][]a = {{2,3 ,4},{4,5,6},{6,7,8},{8,9,3}};
+        //for(int i=0;i<a.length;i++)
+       /* {
+            for (int j=0;j<a[i].length;j++)
+                System.out.println(a[i][j]);
+        }
+    }
+
+}*/
+
